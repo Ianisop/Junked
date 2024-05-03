2f75728d3b26f09de86a53a1d1123418f6c6cf9d
Ianisop <69714456+Ianisop@users.noreply.github.com>
2024-05-03 14:24:30 +0200

    interactor popup

--- Junked!!!/Assets/Scripts/Interactor.cs
+++ Junked!!!/Assets/Scripts/Interactor.cs
@@ -7,55 +7,33 @@ public class Interactor : MonoBehaviour
     //public GameObject currentTarget;
     public float range;
     bool popUp;
-    Interactable interactable;
     public Material outlineMat;
     Material oldMat; // Store the original material
     GameObject currentTarget;
+    public LayerMask layerMask;
+    public GameObject oldTarget;
 
     void Update()
     {
-        return;
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, range))
+        if (Physics.Raycast(ray, out hit, range, layerMask))
         {
-            if (!hit.collider.gameObject.CompareTag("Ground")&& interactable == null)
-            {
-                popUp = true;
-                interactable = hit.collider.gameObject.GetComponent<Interactable>();
-                MeshRenderer meshRenderer = hit.collider.gameObject.GetComponent<MeshRenderer>();
-
-                currentTarget = hit.collider.gameObject;
-
-                // Store the original material if it's not already set
-                if (meshRenderer.sharedMaterial != outlineMat)
-                {
-                    oldMat = meshRenderer.sharedMaterial;
-                    meshRenderer.sharedMaterial = outlineMat; // Apply the outline material
-                }
-
-                 GameManager.Instance.PopUp(interactable.notification);
-               
-
-            }
+            currentTarget = hit.collider.gameObject;
+            Trash trash = hit.collider.gameObject.GetComponent<Trash>();
+            trash.UpdateUI();
+            
+                
         }
         else
         {
-            // Revert to the original material if there was a previous interactable object
-            if (interactable != null)
+            if(currentTarget)
             {
-                interactable.GetComponent<MeshRenderer>().sharedMaterial = oldMat;
-                interactable = null; // Reset interactable reference
+                oldTarget = currentTarget;
+                currentTarget.GetComponent<Trash>().popUp.animator.SetBool("hover", false);
+
+                currentTarget = null;
             }
-            GameManager.Instance.PopUp(" ");
-            currentTarget = null;
-            popUp = false;
-        }
-
-        if (popUp && Input.GetKeyDown(interactable.actionKey))
-        {
-
-            interactable.GetComponent<IInteractableObserver>().Interact();
 
         }
     }
--- Junked!!!/Assets/Scripts/Trash.cs
+++ Junked!!!/Assets/Scripts/Trash.cs
@@ -1,39 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum TrashType
 {
     Metal,
+    Glass,
+    Wood,
     Cardboard,
     Plastic
 }
 
-public class Trash : MonoBehaviour, IInteractableObserver
+
+
+public class Trash : MonoBehaviour
 {
     public static Trash Instance;
     public TrashType trashType;
     public float cleanliness;
     public ParticleSystem particles;
     public int weight;
-
-
+    public PopUp popUp;
+    public string name;
 
     private void Awake()
     {
         if(Instance == this)
             Instance = this; 
         particles = GetComponentInChildren<ParticleSystem>();
+        popUp = this.AddComponent<PopUp>();
+        popUp.Setup();
     }
 
     private void Start()
     {
         cleanliness = Random.Range(4, 10);
         weight = Random.Range(3,15);
+        
     }
 
-    void Update()
+    void Update()   
     {
         if (GameManager.Instance.trashBag.GetComponent<TrashBag>().isOpen && Vector3.Distance(this.transform.position, GameManager.Instance.trashBag.transform.position) <= 1 && GameManager.Instance.dragger.selectedRigidbody != GameManager.Instance.trashBag.GetComponent<Rigidbody>())
         {
@@ -42,8 +51,17 @@ public class Trash : MonoBehaviour, IInteractableObserver
         }
     }
 
-    public void Interact()
+    //called when raycaster looks at it
+    public void UpdateUI()
     {
+        popUp.animator.SetBool("hover", true);
+        popUp.weight = weight;
+        popUp.type = trashType.ToString();
+        popUp.cleanliness = cleanliness;
+        popUp.text.text = popUp.name + "\n" + trashType.ToString() + "\n" + popUp.weight + "\n" + popUp.cleanliness;
+        popUp.canvas.transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward, Camera.main.transform.up);
+       
+       
 
     }
 
