ac833c9be14759fbf657e47e0e1bc2a4821fbabe
Jobaco1 <247365subs@gmail.com>
2024-05-07 11:36:17 +0200

    Added working animated clock in container

--- Junked!!!/Assets/Scripts/LightingManager.cs
+++ Junked!!!/Assets/Scripts/LightingManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-[ExecuteAlways]
 public class LightingManager : MonoBehaviour
 {
     //Scene References
     [SerializeField] private Light DirectionalLight;
     [SerializeField] private LightingPreset Preset;
     //Variables
-    [SerializeField, Range(0, 24)] private float TimeOfDay = 9;
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    [SerializeField] private Animator clockAnimator;
+    [SerializeField] private float timeElapsed = 0;
+    public float startTime = 9f;
 
+    private void Awake()
+    {
+        TimeOfDay = 9;
+    }
 
     private void Update()
     {
@@ -20,23 +27,21 @@ public class LightingManager : MonoBehaviour
 
         // When game plays time will run, otherwise time will stand still
         // but still update if variable is changed
-        if (Application.isPlaying)
-        {
-            TimeOfDay += Time.deltaTime / 60;
-            TimeOfDay %= 24; // Modulus to ensure time always stays between 0-24. Makes it like the clock
-            
-            if (TimeOfDay >= 17) // Restarts the day if is has passed more than "17 o'clock"
-            {
-                print("Day Resetting");
-                TimeOfDay = 9;
-            }
+        TimeOfDay += Time.deltaTime / 60;
+        TimeOfDay %= 24; // Modulus to ensure time always stays between 0-24. Makes it like the clock
 
-            UpdateLighting(TimeOfDay / 24f);
-        }
-        else
+        timeElapsed += Time.deltaTime;
+        clockAnimator.SetFloat("minuteHand", ((timeElapsed % 60) / 60));
+        clockAnimator.SetFloat("hourHand", ((TimeOfDay - startTime) / 8f));
+
+        if (TimeOfDay >= 17) // Restarts the day if is has passed more than "17 o'clock"
         {
-            UpdateLighting(TimeOfDay / 24f);
+            print("Day Resetting");
+            TimeOfDay = startTime;
+            timeElapsed = 0;
         }
+
+        UpdateLighting(TimeOfDay / 24f);
     }
 
     private void UpdateLighting(float timePercent)
