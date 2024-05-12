5bae9fe56cb50f52ff96d7bfe4a2671d0c40a81c
Ianisop <69714456+Ianisop@users.noreply.github.com>
2024-05-12 10:52:30 +0200

    added quota displays, fixed trashbag prefab,

--- Junked!!!/Assets/Scripts/QuotaSystem.cs
+++ Junked!!!/Assets/Scripts/QuotaSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class QuotaSystem : MonoBehaviour
 {
@@ -9,13 +10,28 @@ public class QuotaSystem : MonoBehaviour
     public float co2Quota = 1000;
     public float currentMoney = 0;
     public float currentCO2 = 0;
+    public TMP_Text co2QuotaDisplay, moneyQuotaDisplay;
 
     // Easy = 20, Medium = 16, Hard = 12
     // If slider, between 20 and 10.
     public int difficulty = 16;
 
+    private void Start()
+    {
+        UpdateQuota(0);
+    }
+
     public void UpdateQuota(int day) // Updates the quota and handles random events
     {
+        if(day == 0)
+        {
+            moneyQuota = 100;
+            co2Quota = 1000;
+            co2QuotaDisplay.text = "<size=2>CO2 Quota:\n" + currentCO2 + "kg/" + co2Quota + "kg";
+            moneyQuotaDisplay.text = "<size=2>Money Quota:\n" + currentMoney + "$/" + moneyQuota + "$";
+            return;
+        }
+
         // Just a random chance from 0-100%
         int chance = Random.Range(0, 100);
 
@@ -32,7 +48,9 @@ public class QuotaSystem : MonoBehaviour
             moneyQuota = 100 * (1 + Mathf.Pow(day, 2) / difficulty) * (Random.Range(0, 0.12f) + 1);
             co2Quota = 1000 * (1 + Mathf.Pow(day, 2) / difficulty) * (Random.Range(0, 0.12f) + 1);
             print("Money Quota: " + moneyQuota + " + " + "CO2 Quota " + co2Quota);
-        } 
+        }
+        co2QuotaDisplay.text = "<size=2>CO2 Quota:\n" + currentCO2 + "kg/" + co2Quota + "kg";
+        moneyQuotaDisplay.text = "<size=2>Money Quota:\n" + currentMoney + "$/" + moneyQuota + "$";
     }
 
     public void AddValue(float moneyValue, float co2Value) // Function for adding new values in other scripts
